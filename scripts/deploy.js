17034299c5ad1402bdf2d9a6206a6c3af56fb846
Hanna Drozhdzh <hanna.drozhdzh@gmail.com>
2025-05-23 11:03:41 +0300

    edit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,15 +7,14 @@ async function main() {
 
   const MultiSigWallet = await ethers.getContractFactory("MultiSigWallet");
   const wallet = await MultiSigWallet.deploy(
-    [owner1.address, owner2.address], // владельцы
-    2 // минимальное количество подтверждений
+    [owner1.address, owner2.address],
+    2 
   );
   await wallet.waitForDeployment();
 
   const deployedAddress = await wallet.getAddress();
   console.log("Deployed to:", deployedAddress);
 
-  // Сохраняем адрес в файл
   const addressesPath = path.join(__dirname, "../deployedAddress.json");
   fs.writeFileSync(
     addressesPath,
